67fb6dfcf5451b260a72014aacc957339aa36586
TWeber132 <till-mathis.weber@web.de>
2024-08-09 09:43:53 +0200

    cleanup

--- build_image_kubeflow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-##############################################################################
-##                            Build the image                               ##
-##############################################################################
-docker build \
-  -f kubeflow.Dockerfile \
-  -t thesis/kubeflow-jupyter-tensorflow-full:v1.9.0-rc.2 .
-
